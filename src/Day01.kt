c41f9e3c2f9a1617fae270440a8eac77e7463a7a
dancottam <dancottam@gmail.com>
2021-12-01 22:02:04 +0000

    Day01 part 2 working

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
         var prevDepth: Int? = null
-        var count = 0
+        var numIncreased = 0
         for (line in input) {
             val currentDepth = line.toInt()
             if (prevDepth != null && prevDepth < currentDepth) {
-                count++
+                numIncreased++
             }
             prevDepth = currentDepth
         }
-        return count
+        return numIncreased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var prevSum: Int? = null
+        val last3Measurements = ArrayDeque<Int>(3)
+        var numIncreased = 0
+        for (line in input) {
+            val nextDepth = line.toInt()
+            if (last3Measurements.size < 3) {
+                last3Measurements.add(nextDepth)
+                if (last3Measurements.size < 3) {
+                    continue
+                }
+            } else {
+                last3Measurements.removeFirst()
+                last3Measurements.addLast(nextDepth)
+            }
+            val newSum = last3Measurements.sum()
+            if (prevSum != null && prevSum < newSum) {
+                numIncreased++
+            }
+            prevSum = newSum
+        }
+        return numIncreased
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
