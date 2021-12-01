6d3b1e82bc0cd31f3334883283e9677dfbd5d72b
dancottam <dancottam@gmail.com>
2021-12-01 21:40:28 +0000

    Day01 part 1 working

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prevDepth: Int? = null
+        var count = 0
+        for (line in input) {
+            val currentDepth = line.toInt()
+            if (prevDepth != null && prevDepth < currentDepth) {
+                count++
+            }
+            prevDepth = currentDepth
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +18,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
