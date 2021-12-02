b7638b2aaed4b6f85561f85e2c5d70dbd1df9d4e
dancottam <dancottam@gmail.com>
2021-12-02 14:14:19 +0000

    Add template Day file

---
+++ src/Dayxx.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Dayxx_test")
+    check(part1(testInput) == 1)
+//    check(part2(testInput) == 1)
+
+    val input = readInput("Dayxx")
+//    println(part1(input))
+//    println(part2(input))
+}
