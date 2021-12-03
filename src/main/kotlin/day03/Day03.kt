873f3e985cb2ed762fecd0f2d42203e49a6ecae9
dancottam <dancottam@gmail.com>
2021-12-03 19:13:17 +0000

    Day 03 part 2 working

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -9,42 +9,33 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val report = DiagnosticReport(input)
+        return report.lifeSupportRating
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
+
 
 class DiagnosticReport(input: List<String>) {
     val gammaRate = calculateGammaRate(input)
     val epsilonRate = calculateEpsilonRate(gammaRate)
     val powerConsumption = calculatePowerConsumption(gammaRate, epsilonRate)
+    val oxygenGeneratorRating = calculateOxygenGeneratorRating(input)
+    val co2ScrubberRating = calculateCO2ScrubberRating(input)
+    val lifeSupportRating = calculateLifeSupportRating(oxygenGeneratorRating, co2ScrubberRating)
+
 
     companion object {
-        private fun calculateGammaRate(input: List<String>): String {
+        fun calculateGammaRate(input: List<String>): String {
             return mostCommonBitAtEachPosition(input)
-        }
-
-        private fun calculateEpsilonRate(gammaRate: String): String {
-            val oppositeBits = gammaRate.toCharArray().map { if (it == '0') '1' else '0' }
-            return oppositeBits.joinToString("")
-        }
-
-        private fun calculatePowerConsumption(gammaRate: String, epsilonRate: String): Int {
-            val gammaRateAsInt = convertBinaryToInt(gammaRate)
-            val epsilonRateAsInt = convertBinaryToInt(epsilonRate)
-            return gammaRateAsInt * epsilonRateAsInt
-        }
-
-        private fun convertBinaryToInt(binary: String): Int {
-            return Integer.parseInt(binary, 2)
         }
 
         private fun mostCommonBitAtEachPosition(input: List<String>): String {
@@ -64,17 +55,111 @@ class DiagnosticReport(input: List<String>) {
                     }
                 }
             }
-            val mostCommonBits = reports.values.map { mostCommonBit(it) }
+            val mostCommonBits = reports.values.map { it.mostCommonBit() }
             return mostCommonBits.joinToString("")
         }
 
-        private fun mostCommonBit(it: BitReport): Char {
-            return if (it.zeroes > it.ones) '0' else '1'
+        fun calculateEpsilonRate(gammaRate: String): String {
+            return invertBitsAtEachPosition(gammaRate)
+        }
+
+        private fun invertBitsAtEachPosition(bits: String): String {
+            val oppositeBits = bits.toCharArray().map { if (it == '0') '1' else '0' }
+            return oppositeBits.joinToString("")
+        }
+
+        fun calculatePowerConsumption(gammaRate: String, epsilonRate: String): Int {
+            val gammaRateAsInt = convertBinaryToInt(gammaRate)
+            val epsilonRateAsInt = convertBinaryToInt(epsilonRate)
+            return gammaRateAsInt * epsilonRateAsInt
+        }
+
+        fun calculateOxygenGeneratorRating(input: List<String>): String {
+            var filteredInput = input
+
+            var position = 0
+            while (filteredInput.size > 1) {
+                val mostCommonBit = mostCommonBitAtPosition(filteredInput, position)
+                filteredInput = filterInputByBitAtPosition(filteredInput, position, mostCommonBit)
+                position++
+            }
+            return filteredInput[0]
+        }
+
+        fun calculateCO2ScrubberRating(input: List<String>): String {
+            var filteredInput = input
+
+            var position = 0
+            while (filteredInput.size > 1) {
+                val leastCommonBit = leastCommonBitAtPosition(filteredInput, position)
+                filteredInput = filterInputByBitAtPosition(filteredInput, position, leastCommonBit)
+                position++
+            }
+            return filteredInput[0]
+        }
+
+        private fun mostCommonBitAtPosition(input: List<String>, position: Int): Char {
+            val report = createBitReportForPosition(input, position)
+            return report.mostCommonBit() ?: '1'
+        }
+
+        private fun leastCommonBitAtPosition(input: List<String>, position: Int): Char {
+            val report = createBitReportForPosition(input, position)
+            return report.leastCommonBit() ?: '0'
+        }
+
+        private fun createBitReportForPosition(input: List<String>, position: Int): BitReport {
+            val report = BitReport()
+            for (bits in input) {
+                if (position < bits.length) {
+                    val bit = bits[position]
+                    if (bit == '1') {
+                        report.ones++
+                    } else {
+                        report.zeroes++
+                    }
+                }
+            }
+            return report
+        }
+
+        private fun filterInputByBitAtPosition(input: List<String>, position: Int, bit: Char): List<String> {
+            return input.filter { it[position] == bit }
+        }
+
+        fun calculateLifeSupportRating(oxygenGeneratorRating: String, co2ScrubberRating: String): Int {
+            val oxygenGeneratorRatingAsInt = convertBinaryToInt(oxygenGeneratorRating)
+            val co2ScrubberRatingAsInt = convertBinaryToInt(co2ScrubberRating)
+            return oxygenGeneratorRatingAsInt * co2ScrubberRatingAsInt
+        }
+
+        private fun convertBinaryToInt(binary: String): Int {
+            return Integer.parseInt(binary, 2)
         }
     }
 
-    private data class BitReport (
+    private data class BitReport(
         var zeroes: Int = 0,
         var ones: Int = 0
-    )
+    ) {
+        fun mostCommonBit(): Char? {
+            return if (zeroes == ones) {
+                null
+            } else if (zeroes > ones) {
+                '0'
+            } else {
+                '1'
+            }
+        }
+
+        fun leastCommonBit(): Char? {
+            return if (zeroes == ones) {
+                null
+            } else if (zeroes < ones) {
+                '0'
+            } else {
+                '1'
+            }
+        }
+    }
 }
--- src/test/kotlin/day03/Day03Tests.kt
+++ src/test/kotlin/day03/Day03Tests.kt
@@ -5,7 +5,20 @@ import org.junit.jupiter.api.Test
 
 class Day03Tests {
 
-    private val input = listOf("00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010")
+    private val input = listOf(
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    )
     private val report = DiagnosticReport(input)
 
     @Test
@@ -19,8 +32,23 @@ class Day03Tests {
     }
 
     @Test
-    fun `Power consumption is gamma multiplied by epsilon`() {
+    fun `Power consumption is decimal(gamma) multiplied by decimal(epsilon)`() {
         assertEquals(198, report.powerConsumption)
+    }
+
+    @Test
+    fun `Oxygen generator rating is value that matches the 'most common' bit criteria`() {
+        assertEquals("10111", report.oxygenGeneratorRating)
+    }
+
+    @Test
+    fun `CO2 scrubber rating is the value that matches the 'least common' bit criteria`() {
+        assertEquals("01010", report.co2ScrubberRating)
+    }
+
+    @Test
+    fun `Life support rating is decimal(oxygen generator rating) multiplied by decimal(CO2 scrubber rating)`() {
+        assertEquals(230, report.lifeSupportRating)
     }
 
 }
