fa40e253cf00e74882a87bc70256cf907d099e83
dancottam <dancottam@gmail.com>
2021-12-06 13:09:18 +0000

    Day 06 part 2 working

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -3,40 +3,48 @@ package day06
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val school = School(input[0])
         school.ageByDays(80)
         return school.populationCount()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val school = School(input[0])
+        school.ageByDays(256)
+        return school.populationCount()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5934)
-//    check(part2(testInput) == 1)
+    check(part1(testInput) == 5934L)
+    check(part2(testInput) == 26984457539L)
 
     val input = readInput("Day06")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 class School(input: String) {
 
-    private val lanternFishPopulation = initialiseLanternFish(input)
+    private var numFishAtEachSpawnRate = initializeSpawnRateMap(input)
 
-    private fun initialiseLanternFish(input: String): MutableList<LanternFish> {
-        val lanternFish = mutableListOf<LanternFish>()
-        for (initialSpawnRate in input.split(",")) {
-            lanternFish.add(LanternFish(initialSpawnRate.toInt()))
+    private fun initializeSpawnRateMap(input: String): MutableMap<Int, Long> {
+        val spawnRateMap = mutableMapOf<Int, Long>()
+        for (rate in input.split(",")) {
+            spawnRateMap.merge(rate.toInt(), 1) { oldValue: Long, newValue: Long ->
+                oldValue + newValue
+            }
         }
-        return lanternFish
+        return spawnRateMap
     }
 
-    fun populationCount(): Int {
-        return lanternFishPopulation.size
+    fun numFishPerSpawnRate(): Map<Int, Long> {
+        return numFishAtEachSpawnRate.toSortedMap()
+    }
+
+    fun populationCount(): Long {
+        return numFishAtEachSpawnRate.values.sum()
     }
 
     fun ageByDays(days: Int) {
@@ -46,37 +54,31 @@ class School(input: String) {
     }
 
     private fun ageByOneDay() {
-        val newGeneration = mutableListOf<LanternFish>()
-        for (lanternFish in lanternFishPopulation) {
-            val newLanternFish = lanternFish.ageByOneDayAndCheckSpawn()
-            if (newLanternFish != null) {
-                newGeneration.add(newLanternFish)
+
+        val newSpawnRateCounts = mutableMapOf<Int, Long>()
+
+        for ((rate, numFishAtCurrentRate) in numFishAtEachSpawnRate) {
+            val newRate = calculateNewSpawnRate(rate)
+
+            newSpawnRateCounts.merge(newRate, numFishAtCurrentRate) { existingValue: Long, newValue: Long ->
+                existingValue + newValue
+            }
+
+            if (rate == 0) {
+                newSpawnRateCounts[INITIAL_SPAWN_RATE] = numFishAtCurrentRate
             }
         }
-        lanternFishPopulation.addAll(newGeneration)
-    }
-}
 
-class LanternFish(initialDaysUntilSpawn: Int = DEFAULT_SPAWN_RATE  + 2) {
-
-    private var daysUntilSpawn = initialDaysUntilSpawn
-    val spawnRate = DEFAULT_SPAWN_RATE
-
-    fun daysUntilSpawn(): Int {
-        return daysUntilSpawn
+        numFishAtEachSpawnRate = newSpawnRateCounts
     }
 
-    fun ageByOneDayAndCheckSpawn(): LanternFish? {
-        if (daysUntilSpawn == 0) {
-            daysUntilSpawn = spawnRate
-            return LanternFish()
-        } else {
-            daysUntilSpawn = daysUntilSpawn.dec()
-        }
-        return null
+    private fun calculateNewSpawnRate(rate: Int): Int {
+        return if (rate > 0) rate - 1 else DEFAULT_SPAWN_RATE
     }
+
 
     companion object {
+        const val INITIAL_SPAWN_RATE = 8
         const val DEFAULT_SPAWN_RATE = 6
     }
 }
--- src/test/kotlin/day06/Day06Tests.kt
+++ src/test/kotlin/day06/Day06Tests.kt
@@ -1,46 +1,23 @@
 package day06
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day06Tests {
 
-    val input = "3,4,3,1,2"
-
-    @Test
-    fun `New LanternFish has 8 days until spawn`() {
-        val lanternFish = LanternFish()
-        assertEquals(8, lanternFish.daysUntilSpawn())
-    }
-
-    @Test
-    fun `Days until spawn is decremented when LanternFish ages`() {
-        val lanternFish = LanternFish()
-        lanternFish.ageByOneDayAndCheckSpawn()
-        assertEquals(7, lanternFish.daysUntilSpawn())
-    }
-
-    @Test
-    fun `Days until spawn resets to spawn rate when it reaches zero`() {
-        val lanternFish = LanternFish(0)
-        lanternFish.ageByOneDayAndCheckSpawn()
-
-        assertEquals(lanternFish.spawnRate, lanternFish.daysUntilSpawn())
-    }
-
-    @Test
-    fun `Aging by one day results in new LanternFish only if daysUntilSpawn is 1`() {
-        val lanternFish = LanternFish(8)
-        for (i in 8 downTo 1) {
-            assertNull(lanternFish.ageByOneDayAndCheckSpawn())
-        }
-        assertNotNull(lanternFish.ageByOneDayAndCheckSpawn())
-    }
+    private val input = "3,4,3,1,2"
 
     @Test
     fun `School correctly initialised from input`() {
         val school = School(input)
         assertEquals(5, school.populationCount())
+        assertEquals(
+            sortedMapOf<Int, Long>(
+            1 to 1,
+            2 to 1,
+            3 to 2,
+            4 to 1
+        ), school.numFishPerSpawnRate())
     }
 
     @Test
