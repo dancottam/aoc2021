1bd37e5020d0c944f7af41a0549a0612bde41318
dancottam <dancottam@gmail.com>
2021-12-07 12:09:28 +0000

    Day 07 part 2 working

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -4,46 +4,92 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val crabSubs = CrabSubs(input[0])
-        return crabSubs.cheapestFuelSpend()
+        val crabSubs = CrabSubs.Part1(input[0])
+        return crabSubs.cheapestFuelSpend() ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val crabSubs = CrabSubs.Part2(input[0])
+        return crabSubs.cheapestFuelSpend() ?: 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 37)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day07")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
-class CrabSubs(
+abstract class CrabSubs(
     input: String
 ) {
-    private val positions = input.split(",").map { it.toInt() }.toList()
+    val positions = input.split(",").map { it.toInt() }.toList().sorted()
 
-    fun fuelSpendForPosition(target: Int): Int {
-        var fuelSpend = 0
-        for (position in positions) {
-            if (position < target) {
-                fuelSpend += target - position
-            } else if (position > target) {
-                fuelSpend += position - target
-            }
-        }
-        return fuelSpend
+    abstract fun fuelSpendForPosition(target: Int): Int
+
+    fun cheapestPosition(): Int? {
+        return cheapestPositionSpend()?.position
     }
 
-    fun cheapestFuelSpend(): Int {
-        val spendPerPosition = mutableMapOf<Int, Int>()
-        for (position in positions) {
-            spendPerPosition[position] = fuelSpendForPosition(position)
+    fun cheapestFuelSpend(): Int? {
+        return cheapestPositionSpend()?.spend
+    }
+
+    private fun cheapestPositionSpend(): PositionSpend? {
+        var cheapestPositionSpend: PositionSpend? = null
+        for (position in 0..positions.last()) {
+            val positionSpend = PositionSpend(position, fuelSpendForPosition(position))
+            if (cheapestPositionSpend == null || cheapestPositionSpend.spend > positionSpend.spend) {
+                cheapestPositionSpend = positionSpend
+            }
         }
-        return spendPerPosition.toList().minByOrNull { (_, value) -> value }!!.second
+        return cheapestPositionSpend
+    }
+
+    private data class PositionSpend(
+        val position: Int,
+        val spend: Int
+    )
+
+    class Part1(input: String) : CrabSubs(input = input) {
+
+        override fun fuelSpendForPosition(target: Int): Int {
+            var fuelSpend = 0
+            for (position in positions) {
+                if (position < target) {
+                    fuelSpend += target - position
+                } else if (position > target) {
+                    fuelSpend += position - target
+                }
+            }
+            return fuelSpend
+        }
+
+    }
+
+    class Part2(input: String) : CrabSubs(input = input) {
+
+        override fun fuelSpendForPosition(target: Int): Int {
+            var fuelSpend = 0
+            for (position in positions) {
+                var distance = 0
+                if (position < target) {
+                    distance = target - position
+                } else if (position > target) {
+                    distance = position - target
+                }
+                if (distance > 0) {
+                    var stepCost = 1
+                    for (i in 1..distance) {
+                        fuelSpend += stepCost
+                        stepCost++
+                    }
+                }
+            }
+            return fuelSpend
+        }
     }
 }
--- src/test/kotlin/day07/Day07Tests.kt
+++ src/test/kotlin/day07/Day07Tests.kt
@@ -7,18 +7,46 @@ class Day07Tests {
 
     private val input = "16,1,2,0,4,2,7,1,2,14"
 
-    private val crabSubs = CrabSubs(input)
-
     @Test
-    fun `Calculate fuel spend for position`() {
-        assertEquals(41, crabSubs.fuelSpendForPosition(1))
-        assertEquals(39, crabSubs.fuelSpendForPosition(3))
-        assertEquals(71, crabSubs.fuelSpendForPosition(10))
+    fun `Part 1 calculate fuel spend for position`() {
+        val part1CrabSubs = CrabSubs.Part1(input)
+        assertEquals(41, part1CrabSubs.fuelSpendForPosition(1))
+        assertEquals(39, part1CrabSubs.fuelSpendForPosition(3))
+        assertEquals(71, part1CrabSubs.fuelSpendForPosition(10))
     }
 
     @Test
-    fun `Calculate cheapest fuel spend`() {
-        assertEquals(37, crabSubs.cheapestFuelSpend())
+    fun `Part 1 cheapest position`() {
+        assertEquals(2, CrabSubs.Part1(input).cheapestPosition())
+    }
+
+    @Test
+    fun `Part 1 calculate cheapest fuel spend`() {
+        assertEquals(37, CrabSubs.Part1(input).cheapestFuelSpend())
+    }
+
+    @Test
+    fun `Part 2 calculate fuel spend for position`() {
+        assertEquals(66, CrabSubs.Part2("16").fuelSpendForPosition(5))
+        assertEquals(10, CrabSubs.Part2("1").fuelSpendForPosition(5))
+        assertEquals(6, CrabSubs.Part2("2").fuelSpendForPosition(5))
+        assertEquals(15, CrabSubs.Part2("0").fuelSpendForPosition(5))
+        assertEquals(1, CrabSubs.Part2("4").fuelSpendForPosition(5))
+        assertEquals(6, CrabSubs.Part2("2").fuelSpendForPosition(5))
+        assertEquals(3, CrabSubs.Part2("7").fuelSpendForPosition(5))
+        assertEquals(10, CrabSubs.Part2("1").fuelSpendForPosition(5))
+        assertEquals(6, CrabSubs.Part2("2").fuelSpendForPosition(5))
+        assertEquals(45, CrabSubs.Part2("14").fuelSpendForPosition(5))
+    }
+
+    @Test
+    fun `Part 2 cheapest position`() {
+        assertEquals(5, CrabSubs.Part2(input).cheapestPosition())
+    }
+
+    @Test
+    fun `Part 2 calculate cheapest fuel spend`() {
+        assertEquals(168, CrabSubs.Part2(input).cheapestFuelSpend())
     }
 
 }
