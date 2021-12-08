f09721d2d7e8233d5f6b7f3b732d3861c1c5e27d
dancottam <dancottam@gmail.com>
2021-12-08 16:11:50 +0000

    Day 08 part 2 working

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -1,6 +1,7 @@
 package day08
 
 import readInput
+import java.util.*
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -8,18 +9,19 @@ fun main() {
         return notes.countInstancesOfNumbersInOutput(listOf(1, 4, 7, 8))
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val notes = Notes(input)
+        return notes.sumAllOutputValues()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 26)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 61229L)
 
     val input = readInput("Day08")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 class Notes (input: List<String>) {
@@ -39,6 +41,14 @@ class Notes (input: List<String>) {
         }
         return total
     }
+
+    fun sumAllOutputValues(): Long {
+        var total = 0L
+        for (entry in entries) {
+            total = total.plus(entry.outputValue)
+        }
+        return total
+    }
 }
 
 class Entry(input: String) {
@@ -46,29 +56,96 @@ class Entry(input: String) {
     val signalPatterns = parseSignalPatterns(input)
     val outputPatterns = parseOutputPatterns(input)
 
-    private fun parseSignalPatterns(input: String): List<String> {
-        return input.split("|")[0].trim().split(" ").toList()
-    }
+    private val numberPatterns = createNumberPatternMap()
+    val outputValue = calculateOutputValue()
 
-    private fun parseOutputPatterns(input: String): List<String> {
-        return input.split("|")[1].trim().split(" ").toList()
-    }
-
-    fun getPatternForNumber(number: Int): String {
-        if (number == 1) {
-            return signalPatterns.first { it.length == 2 }.toSortedSet().joinToString("")
-        } else if (number == 4) {
-            return signalPatterns.first { it.length == 4 }.toSortedSet().joinToString("")
-        } else if (number == 7) {
-            return signalPatterns.first { it.length == 3 }.toSortedSet().joinToString("")
-        } else if (number == 8) {
-            return signalPatterns.first { it.length == 7 }.toSortedSet().joinToString("")
+    private fun parseSignalPatterns(input: String): List<SortedSet<Char>> {
+        return input.split("|")[0].trim().split(" ").map {
+            it.toSortedSet()
         }
-        TODO("Not yet implemented")
+    }
+
+    private fun parseOutputPatterns(input: String): List<SortedSet<Char>> {
+        return input.split("|")[1].trim().split(" ").map {
+            it.toSortedSet()
+        }
+    }
+
+    private fun createNumberPatternMap(): Map<Int, SortedSet<Char>> {
+        val numberPatterns = mutableMapOf<Int, SortedSet<Char>>()
+
+        val onePattern = signalPatterns.first { it.size == 2 }
+        numberPatterns[1] = onePattern
+        val fourPattern = signalPatterns.first { it.size == 4 }
+        numberPatterns[4] = fourPattern
+        val sevenPattern = signalPatterns.first { it.size == 3 }
+        numberPatterns[7] = sevenPattern
+        val eightPattern = signalPatterns.first { it.size == 7 }
+        numberPatterns[8] = eightPattern
+
+        val zeroPattern = signalPatterns.first { segments ->
+            segments.size == 6
+                    && segments.containsAll(sevenPattern)
+                    && !segments.containsAll(fourPattern)
+        }
+        numberPatterns[0] = zeroPattern
+
+        val ninePattern = signalPatterns.first { segments ->
+            segments.size == 6
+                    && segments.containsAll(fourPattern)
+                    && !segments.containsAll(eightPattern)
+        }
+        numberPatterns[9] = ninePattern
+
+        val threePattern = signalPatterns.first { segments ->
+            segments.size == 5
+                    && segments.containsAll(onePattern)
+                    && ninePattern.containsAll(segments)
+        }
+        numberPatterns[3] = threePattern
+
+        val fivePattern = signalPatterns.first { segments ->
+            segments.size  == 5
+                    && !segments.containsAll(onePattern)
+                    && ninePattern.containsAll(segments)
+        }
+        numberPatterns[5] = fivePattern
+
+        val sixPattern = signalPatterns.first { segments ->
+            segments.size == 6
+                    && segments.containsAll(fivePattern)
+                    && !segments.containsAll(onePattern)
+        }
+        numberPatterns[6] = sixPattern
+
+        val twoPattern = signalPatterns.first { segments ->
+            segments.size == 5
+                    && segments != threePattern
+                    && segments != fivePattern
+        }
+        numberPatterns[2] = twoPattern
+
+        return numberPatterns.toSortedMap()
+    }
+
+    fun patternForNumber(number: Int): String {
+        return numberPatterns[number]?.joinToString("") ?: ""
     }
 
     fun countInstancesOfNumberInOutput(number: Int): Int {
-        val pattern = getPatternForNumber(number)
-        return outputPatterns.count { it.toSortedSet().joinToString("") == pattern }
+        val pattern = patternForNumber(number)
+        return outputPatterns.count { it.joinToString("") == pattern }
+    }
+
+    private fun calculateOutputValue(): Int {
+        var outputValue = ""
+        for (pattern in outputPatterns) {
+            outputValue += numberForPattern(pattern)
+        }
+        return outputValue.toInt()
+    }
+
+    private fun numberForPattern(pattern: SortedSet<Char>): Int {
+        return numberPatterns.filterValues { it == pattern }.keys.first()
     }
 }
--- src/test/kotlin/day08/Day08Tests.kt
+++ src/test/kotlin/day08/Day08Tests.kt
@@ -12,20 +12,39 @@ class Day08Tests {
     fun `Read input`() {
         val entry = Entry(input)
         assertEquals(listOf(
-            "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"
+            "acedgfb".toSortedSet(),
+            "cdfbe".toSortedSet(),
+            "gcdfa".toSortedSet(),
+            "fbcad".toSortedSet(),
+            "dab".toSortedSet(),
+            "cefabd".toSortedSet(),
+            "cdfgeb".toSortedSet(),
+            "eafb".toSortedSet(),
+            "cagedb".toSortedSet(),
+            "ab".toSortedSet()
         ), entry.signalPatterns)
         assertEquals(listOf(
-            "cdfeb", "fcadb", "cdfeb", "cdbaf"
+            "cdfeb".toSortedSet(),
+            "fcadb".toSortedSet(),
+            "cdfeb".toSortedSet(),
+            "cdbaf".toSortedSet()
         ), entry.outputPatterns)
     }
 
     @Test
-    fun `Find patterns for easy numbers`() {
+    fun `Find patterns for numbers`() {
         val entry = Entry(input)
-        assertEquals("ab", entry.getPatternForNumber(1))
-        assertEquals("abef", entry.getPatternForNumber(4))
-        assertEquals("abd", entry.getPatternForNumber(7))
-        assertEquals("abcdefg", entry.getPatternForNumber(8))
+        assertEquals("ab", entry.patternForNumber(1))
+        assertEquals("abef", entry.patternForNumber(4))
+        assertEquals("abd", entry.patternForNumber(7))
+        assertEquals("abcdefg", entry.patternForNumber(8))
+
+        assertEquals("abcdeg", entry.patternForNumber(0))
+        assertEquals("abcdef", entry.patternForNumber(9))
+        assertEquals("abcdf", entry.patternForNumber(3))
+        assertEquals("bcdef", entry.patternForNumber(5))
+        assertEquals("bcdefg", entry.patternForNumber(6))
+        assertEquals("acdfg", entry.patternForNumber(2))
     }
 
     @Test
@@ -41,6 +60,18 @@ class Day08Tests {
     fun `Count numbers in output patterns`() {
         val notes = Notes(readInput("Day08_test"))
         assertEquals(26, notes.countInstancesOfNumbersInOutput(listOf(1, 4, 7, 8)))
+    }
+
+    @Test
+    fun `Determine output value`() {
+        val entry = Entry(input)
+        assertEquals(5353, entry.outputValue)
+    }
+
+    @Test
+    fun `Sum all output values`() {
+        val notes = Notes(readInput("Day08_test"))
+        assertEquals(61229, notes.sumAllOutputValues())
     }
 
 }
